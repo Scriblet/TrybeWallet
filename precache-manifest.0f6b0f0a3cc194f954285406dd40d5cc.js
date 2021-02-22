056d8d95db7f485b672c28c8e20cfd743588497c
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-02-22 08:06:03 -0300

    Updates

---
+++ precache-manifest.0f6b0f0a3cc194f954285406dd40d5cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9279b0928a9d6a263c7e85c6f2a09ac",
+    "url": "/TrybeWallet/index.html"
+  },
+  {
+    "revision": "590ac08f67229ed4dd64",
+    "url": "/TrybeWallet/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "4418f68a0a49b92b330d",
+    "url": "/TrybeWallet/static/css/main.be8241e6.chunk.css"
+  },
+  {
+    "revision": "590ac08f67229ed4dd64",
+    "url": "/TrybeWallet/static/js/2.b20ac824.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/TrybeWallet/static/js/2.b20ac824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4418f68a0a49b92b330d",
+    "url": "/TrybeWallet/static/js/main.5b9e1344.chunk.js"
+  },
+  {
+    "revision": "0246e2d5b4e5122b57b9",
+    "url": "/TrybeWallet/static/js/runtime-main.ed03227f.js"
+  },
+  {
+    "revision": "17fb69ad464ba4f1f84e1d59380abd2f",
+    "url": "/TrybeWallet/static/media/logo.17fb69ad.webp"
+  }
+]);
